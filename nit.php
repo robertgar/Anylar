262b76b36a909fb4b05a82d1fc1e93fbdaea0c29
robertgar <50602861+robertgar@users.noreply.github.com>
2021-07-31 22:03:46 -0600

    Update nit.php

--- nit.php
+++ nit.php
@@ -1,24 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
-<form method="POST">
-	<label for="direccion"></label>
-    <input type="text" name="direccion" value="">
-<button type="submit" name="buton">Enviar</button>
-</form>
-<h1></h1>
-<p></p>
-
-
-</body>
-</html>
-<?php
-if(isset($_POST['direccion']))
-{
-$a=$_POST['direccion'];
+<?php 
+  header('Access-Control-Allow-Origin: *'); 
+  header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+  
+  $json = file_get_contents('php://input');
+$direc='';
+$nombre='';
+$a=  json_decode($json);
 
 $buffer='<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
 xmlns:ser="http://services.ws.ingface.com/">
@@ -34,12 +21,7 @@ xmlns:ser="http://services.ws.ingface.com/">
 </soapenv:Body';
 
 $url ="https://www.ingface.net/ServiciosIngface/ingfaceWsServices";  //URL de DHL
-
-
 $prexml = $buffer;
-
-
-
 $soap_do = curl_init(); 
 curl_setopt($soap_do, CURLOPT_URL,            $url );   
 curl_setopt($soap_do, CURLOPT_CONNECTTIMEOUT, 10); 
@@ -51,59 +33,38 @@ curl_setopt($soap_do, CURLOPT_POST,           true );
 curl_setopt($soap_do, CURLOPT_POSTFIELDS,    $prexml); 
 curl_setopt($soap_do, CURLOPT_HTTPHEADER, array("Content-Type: text/xml"));
 
-
 $result = curl_exec($soap_do);
 $err = curl_error($soap_do);
-       
 
-            // Verificar si ocurrió algún error
+           // Verificar si ocurrió algún error
 if (curl_exec($soap_do) === false) {
-
-echo ' ' . $a.' ';
-
 echo 'Curl error: ' . curl_error($soap_do);
-
-
 } else {
- 
-echo ' ' . $a.' ';
-echo '<br>';
-
-echo '<br>';
 $response = preg_replace("/(<\/?)(\w+):([^>]*>)/", "$1$2$3", $result);
 $xml = new SimpleXMLElement($response);
 $body = $xml->xpath('//SBody')[0];
 $array = json_decode(json_encode((array)$body), TRUE); 
-//print_r($array);
-
-  
- echo utf8_decode($array['ns2nitContribuyentesResponse']['return']['direccion_completa']);
- echo '<br>';
- echo '<br>';
+ $direc= utf8_decode($array['ns2nitContribuyentesResponse']['return']['direccion_completa']);
  $aaaa= utf8_decode($array['ns2nitContribuyentesResponse']['return']['nombre']);
-
-  echo '<br>';
- echo '<br>';
 $div=explode( ',,', $aaaa  );
-
 $tl= $div[1].' '.$div[0]; 
-$salida = str_replace(',', '  ', $tl);
-echo $salida;
-
-
-
-echo '<br>';
-
-
-
-
-
-
+$nombre = utf8_decode(str_replace(',', '  ', $tl));
 }
-
-
 curl_close ($soap_do);
 
+  class Result {}
 
-}
+
+
+
+
+  $response = new Result();
+  $response->resultado = 'OK';
+  $response->mensaje = $direc;
+  $response->mensaje1 = $nombre;
+
+  header('Content-Type: application/json');
+  echo json_encode($response);  
+
+
 ?>
