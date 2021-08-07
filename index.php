6cea0a56157e9fbe7630c2573241e6363b2059e1
robertgar <50602861+robertgar@users.noreply.github.com>
2021-08-06 21:09:02 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,94 +1,19 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>Page Title</title>
-</head>
-<body>
-<form method="POST">
-	<label for="direccion"></label>
-    <input type="text" name="direccion" value="">
-<button type="submit" name="buton">Enviar</button>
-</form>
-<h1></h1>
-<p></p>
-
-
-</body>
-</html>
 <?php
-if(isset($_POST['direccion']))
-{
-$a=$_POST['direccion'];
+$servername = "SQL5102.site4now.net";
+$database = "db_a54053_bdprueba";
+$username = "db_a54053_bdprueba_admin";
+$password = "Marlon1989";
 
-$buffer='<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/"
-xmlns:ser="http://services.ws.ingface.com/">
-<soapenv:Header/>
-<soapenv:Body>
-<ser:nitContribuyentes>
-<!--Optional:-->
-<usuario>DEMO</usuario>
-<!--Optional:-->
-<clave>C2FDC80789AFAF22C372965901B16DF533A4FCB19FD9F2FD5CBDA554032983B0</clave>
-<nit>'.$a.'</nit>
-</ser:nitContribuyentes>
-</soapenv:Body';
+$connectionInfo = array( "Database"=>$database, "UID"=>$username, "PWD"=>$password);
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
 
-$url ="https://www.ingface.net/ServiciosIngface/ingfaceWsServices";  //URL de DHL
-
-
-$prexml = $buffer;
-
-
-
-$soap_do = curl_init(); 
-curl_setopt($soap_do, CURLOPT_URL,            $url );   
-curl_setopt($soap_do, CURLOPT_CONNECTTIMEOUT, 10); 
-curl_setopt($soap_do, CURLOPT_TIMEOUT,        10); 
-curl_setopt($soap_do, CURLOPT_RETURNTRANSFER, true );
-curl_setopt($soap_do, CURLOPT_SSL_VERIFYPEER, false);  
-curl_setopt($soap_do, CURLOPT_SSL_VERIFYHOST, false); 
-curl_setopt($soap_do, CURLOPT_POST,           true ); 
-curl_setopt($soap_do, CURLOPT_POSTFIELDS,    $prexml); 
-curl_setopt($soap_do, CURLOPT_HTTPHEADER, array("Content-Type: text/xml"));
-
-
-$result = curl_exec($soap_do);
-$err = curl_error($soap_do);
-       
-
-            // Verificar si ocurrió algún error
-if (curl_exec($soap_do) === false) {
-
-echo ' ' . $a.' ';
-
-echo 'Curl error: ' . curl_error($soap_do);
-
-
-} else {
- 
-echo ' ' . $a.' ';
-echo '<br>';
-
-echo '<br>';
-$response = preg_replace("/(<\/?)(\w+):([^>]*>)/", "$1$2$3", $result);
-$xml = new SimpleXMLElement($response);
-$body = $xml->xpath('//SBody')[0];
-$array = json_decode(json_encode((array)$body), TRUE); 
-//print_r($array);
-
- echo utf8_decode($array['ns2nitContribuyentesResponse']['return']['direccion_completa']);
- echo '<br>';
- $aaaa= utf8_decode($array['ns2nitContribuyentesResponse']['return']['nombre']);
-$div=explode( ',,', $aaaa  );
-$tl= $div[1].' '.$div[0]; 
-$salida = str_replace(',', '  ', $tl);
- echo '<br>';	
-echo $salida;
+if( $conn ) {
+     echo "Conexión establecida.<br />";
+}else{
+     echo "Conexión no se pudo establecer.<br />";
+     die( print_r( sqlsrv_errors(), true));
 }
+//echo "conexion satisfacotria";
+//mysqli_close($con);
 
-
-curl_close ($soap_do);
-
-
-}
 ?>
